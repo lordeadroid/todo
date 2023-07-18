014b67479875108ba5f49c3dcb1f268d5b75e439
Rishabh <lordeadroid@users.noreply.github.com>
2023-07-18 17:51:47 +0530

    added styling to the page

--- todo.js
+++ todo.js
@@ -2,17 +2,17 @@ const resetTaskBoxValue = (element) => {
   element.value = "";
 };
 
+const appendElement = (taskElement) => {
+  const page = document.querySelector(".page");
+
+  page.appendChild(taskElement);
+};
+
 const createTask = (task) => {
   const taskElement = document.createElement("li");
   taskElement.innerText = task;
 
   appendElement(taskElement);
-};
-
-const appendElement = (taskElement) => {
-  const page = document.querySelector(".page");
-
-  page.appendChild(taskElement);
 };
 
 const takeTask = (cb) => {
@@ -22,24 +22,13 @@ const takeTask = (cb) => {
   cb(task);
 };
 
-const markUndo = () => {
-  const tasks = document.querySelectorAll(".done");
-  console.log(tasks);
-  tasks.forEach((task) => {
-    task.onclick = () => {
-      task.classList.remove("done");
-    };
-  });
-};
-
-const markCompleteTask = () => {
+const markCompletedTask = () => {
   const tasks = document.querySelectorAll("li");
   tasks.forEach((task) => {
     task.onclick = () => {
       task.classList.add("done");
     };
   });
-  markUndo();
 };
 
 const main = () => {
@@ -47,7 +36,7 @@ const main = () => {
 
   saveButton.onclick = () => {
     takeTask(createTask);
-    markCompleteTask();
+    markCompletedTask();
   };
 };
 
