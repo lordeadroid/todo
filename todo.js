c1394f2f83545f53b073afa247eb85dd3cb7634d
Rishabh <lordeadroid@users.noreply.github.com>
2023-07-19 18:59:58 +0530

    add TodoViewer class

--- todo.js
+++ todo.js
@@ -42,23 +42,76 @@ class TodoList {
   }
 }
 
+class TodoViewer {
+  #todoList;
+
+  constructor(todoList) {
+    this.#todoList = todoList;
+  }
+
+  get render() {
+    this.#removeTasks();
+
+    const tasks = this.#todoList.allTodos;
+    tasks.forEach((todo) => {
+      const taskElement = this.#createTaskElement(todo);
+      this.#renderTask(taskElement);
+    });
+  }
+
+  #removeTasks() {
+    const tasks = document.querySelector("#tasks");
+
+    while (tasks.hasChildNodes()) {
+      tasks.removeChild(tasks.firstChild);
+    }
+  }
+
+  #createTaskElement(todo) {
+    const taskElement = document.createElement("li");
+    taskElement.innerText = todo.description;
+    taskElement.setAttribute("id", `task-${todo.id}`);
+    return taskElement;
+  }
+
+  #renderTask(taskElement) {
+    const page = document.querySelector("#tasks");
+    page.appendChild(taskElement);
+  }
+}
+
 const readTask = (taskBox) => {
   const task = taskBox.value;
   taskBox.value = "";
   return task;
 };
 
+class GenerateId {
+  #idNumber;
+
+  constructor() {
+    this.#idNumber = 0;
+  }
+
+  get number() {
+    this.#idNumber += 1;
+    return this.#idNumber;
+  }
+}
+
 const main = () => {
   const taskBox = document.querySelector("#task-box");
   const saveButton = document.querySelector("#save-button");
   const todoList = new TodoList();
-
-  const id = 0;
+  const generateId = new GenerateId();
 
   saveButton.onclick = () => {
     const task = readTask(taskBox);
-    const todo = new Todo(id, task);
+    const todo = new Todo(generateId.number, task);
     todoList.add(todo);
+
+    const todoViewer = new TodoViewer(todoList);
+    todoViewer.render;
   };
 };
 
