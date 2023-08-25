bdb750018b8b7c3045e8bcacb4c0e6ab15351cbb
Rishabh <lordeadroid@users.noreply.github.com>
2023-08-25 15:19:39 +0530

    Test: Add API test for adding todo

--- test/app-test.js
+++ test/app-test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const { describe, it } = require('node:test');
 const { createApp } = require('../src/app');
+const { Todo } = require('../src/models/todo');
 const { TodoList } = require('../src/models/todo-list');
 const { TodoLists } = require('../src/models/todo-lists');
 
@@ -38,12 +39,32 @@ describe('APP', () => {
       const todoLists = new TodoLists();
       const app = createApp(todoLists);
       const listName = 'work';
+
       request(app)
         .post('/todo-lists')
         .send({ listName })
         .expect(201)
         .expect('content-type', /json/)
         .expect({ listId: 0 })
+        .end(done);
+    });
+  });
+
+  describe('POST /todo-lists/:listId', () => {
+    it('should add a todo to the given list', (_, done) => {
+      const todoDescription = 'drink water';
+      const listId = 0;
+      const todoList = new TodoList('work', listId);
+      const todoLists = new TodoLists();
+      todoLists.addTodoList(todoList);
+      const app = createApp(todoLists);
+
+      request(app)
+        .post(`/todo-lists/${listId}`)
+        .send({ todoDescription })
+        .expect(201)
+        .expect('content-type', /json/)
+        .expect({ todoId: 0 })
         .end(done);
     });
   });
