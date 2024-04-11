49ce0d3e2d996aba3811e94894459b34ee8ecd41
lordeadroid <rishabhrawat34@gmail.com>
2024-04-11 13:22:35 +0530

    feat: attached event listener on add button

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,15 @@
 const main = () => {
-  const addListBox = document.getElementById('add-list-box');
-  const addListButton = document.getElementById('add-list-button');
-  const todoListContainer = document.getElementById('todo-list');
+  const addListBox = document.getElementById("add-list-box");
+  const addListButton = document.getElementById("add-list-button");
+  const todoListContainer = document.getElementById("todo-list");
+  const showAddList = document.querySelector(".show-add-list");
 
-  const todoView = new TodoView(addListBox, addListButton, todoListContainer);
+  const todoView = new TodoView(
+    addListBox,
+    addListButton,
+    todoListContainer,
+    showAddList
+  );
   const todoLists = new TodoLists();
   const todoAdmin = new TodoAdmin(todoLists);
   const todoController = new TodoController(todoView, todoAdmin);
